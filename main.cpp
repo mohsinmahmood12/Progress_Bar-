3d7e8c667e9c3a88bfef09858c75981c055c59cf
Mohsin Mehmood <55545648+mohsinmahmood12@users.noreply.github.com>
2021-08-27 22:30:55 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,35 @@
+#include<cmath>
+#include<iomanip>
+#include<string>
+#include<iostream>
+#include<thread>
+
+void show_progress_bar(std::ostream &os,int time,
+                   std::string message,char symbol = '*')
+{
+    static const auto bar_length = 70;
+    if(message.length() >= bar_length){
+        os << message <<"\n";
+        message.clear();
+    }else{
+        message+=" ";
+    }
+
+    const auto progress_level = 100.0/(bar_length - message.length());
+
+    std::cout<<message;
+
+    for(double percentage=0;percentage<=101;percentage+=progress_level)
+    {
+        message+=symbol;
+        os << "\r ["<<std::setw(3)<<static_cast<int>(percentage)<<"% ] "<<message<<std::flush;
+        std::this_thread::sleep_for(std::chrono::milliseconds(time));
+    }
+    os <<"\n\n";
+
+}
+int main()
+{
+    show_progress_bar(std::clog,100,"progress",'#');
+}
 
